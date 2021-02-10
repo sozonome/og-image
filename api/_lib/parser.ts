7e95f6cf78c802c65f1728036b259efa652cba08
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-02-10 22:17:29 +0700

    unlock assets from outside vercel 😈

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -51,9 +51,10 @@ function getArray(stringOrArray: string[] | string | undefined): string[] {
 }
 
 function getDefaultImages(images: string[], theme: Theme): string[] {
-    const defaultImage = theme === 'light'
-        ? 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg'
-        : 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg';
+    // const defaultImage = theme === 'light'
+    //     ? 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg'
+    //     : 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg';
+    const defaultImage = 'https://sznm.dev/avataaars.svg'
 
     if (!images || !images[0]) {
         return [defaultImage];
